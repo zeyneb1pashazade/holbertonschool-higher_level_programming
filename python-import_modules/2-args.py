98ea4e3fb268cc21d82da280ec08f9e12376ee0c
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-19 13:18:18 +0000

    python3/2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    a = sys.argv
+    b = len(argv) - 1
+    if b == 0:
+        print("0 arguments.")
+    elif b == 1:
+        print("1 argument:")
+    else :
+        print(f"{b} arguments:")
+    for i in range(1:b+1):
+        print(f"{i}: argv[i]")
