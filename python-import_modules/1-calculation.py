fb6c357f3cd9c6ed1f46363dde0c965f3e28c3ab
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-19 12:15:15 +0000

    python3/2

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
