08d90b4d9936d48c24b392addc6b755b60ee8277
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-19 13:24:13 +0000

    python3/2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
     a = sys.argv
-    b = len(argv) - 1
+    b = len(a) - 1
     if b == 0:
         print("0 arguments.")
     elif b == 1:
         print("1 argument:")
     else :
         print(f"{b} arguments:")
-    for i in range(1:b+1):
-        print(f"{i}: argv[i]")
+    for i in range(1, b + 1):
+        print(f"{i}: {a[i]}")
