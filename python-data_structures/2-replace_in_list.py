c52e9de985ac04a25eb4cfa3451634ab40f3ad3c
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-22 16:59:40 +0000

    replace element

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if ((adx < 0)) or ((idx >= len(my_list))):
+        return my_list
+    else:
+        return my_list[idx] = new_element
