22d65b1725bf1b5c127b11ef122fc7c75622d8f0
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-22 16:24:40 +0000

    retrivate element from list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+â#!/usr/bin/python3
+def element_at(my_list, idx):
+    if ((idx < 0)):
+        return "None"
+    elif ((idx > len(my_list))):
+        return "None"
+    else:
+        return my_list[idx]
+
