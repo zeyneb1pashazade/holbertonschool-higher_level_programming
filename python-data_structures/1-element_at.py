d2c3f3700f210494dc4c439f017f6772538dd782
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-22 16:33:54 +0000

    python

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if ((idx < 0)):
-        return "None"
+        return None
     elif ((idx > len(my_list))):
-        return "None"
+        return None
     else:
         return my_list[idx]
 
