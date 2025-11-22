db417b513856d1f7db35a20b7e909194e7b68979
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-22 19:30:53 +0000

    i hate to see C in my code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-new_string = my_string
 def no_c(my_string):
+    new_string = ""
     for i in my_string:
-        if ((i == "c")) or ((i == "C")):
-            new_string = new_string + i
+        if ((i != "c")) and ((i != "C")):
+                new_string = new_string + i
     return new_string
