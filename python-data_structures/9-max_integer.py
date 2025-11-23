32499acc65a70ac037700332820c7dfd9afed642
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-23 08:38:53 +0000

    python2

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-'''
- Find the biggest integer of a list.
- If the list is empty, function will return None.
-'''
     if not my_list:
         return None
     biggest = my_list[0]
