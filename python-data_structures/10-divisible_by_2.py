baef457d6c34dadc2a73bdd1bdb7cb66f5b95e09
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-23 08:46:15 +0000

    python2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for element in my_list:
         if element % 2 == 0:
-            new_list += True
+            new_list += 'True'
         else:
-            new_list += False
+            new_list += 'False'
     return new_list
