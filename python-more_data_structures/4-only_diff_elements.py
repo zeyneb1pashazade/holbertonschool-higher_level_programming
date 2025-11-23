46cb529bbef9757aa2cacc6be42d41cda3a3b0d3
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-23 11:39:04 +0000

    more pythonstructures

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     new_set = []
     for i in set_1:
-        if not i in set_2:
+        if i not in set_2:
             new_set.append(i)
     for j in set_2:
-        if not j in set_1:
+        if j not in set_1:
             new_set.append(j)
     return new_set
