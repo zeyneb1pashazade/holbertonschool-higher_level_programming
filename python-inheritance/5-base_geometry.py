dd90aeff93f228035ac5c4817db2b6dce262b7f2
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-29 18:33:39 +0000

    python inheritence

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py: Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class serving as the base for geometric objects.
+    """
+    pass
