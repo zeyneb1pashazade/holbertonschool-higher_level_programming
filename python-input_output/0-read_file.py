6a1340320181266b8e7f4eb51604b7cacbafa54f
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-29 20:25:54 +0000

    python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 '''function that reads a text file (UTF8) and prints it to stdout'''
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints its content"""
-    with open(my_file_0.txt, encoding="utf-8") as f:
-        print(f.read(), end="") 
+    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+        print(f.read(), end="")
