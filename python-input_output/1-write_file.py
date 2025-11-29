5ceaa36750cf28104098a4b656bc3fab93e47cf8
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-29 20:43:03 +0000

    python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
-'''function that writes a string to a text file (UTF8) and returns the number of characters written'''
+"""
+1-write_file.py
+
+A function that writes a string to a text file (UTF8) and returns
+the number of characters written.
+"""
 
 
 def write_file(filename="", text=""):
-    # Open the file using the 'with' statement.
+    """
+    Writes a string to a text file, creating it if it doesn't exist,
+    or overwriting it if it does.
+
+    The file is opened in 'w' (write) mode with UTF-8 encoding.
+
+    Args:
+        filename (str): The name of the file to write to. Defaults to "".
+        text (str): The string content to write to the file. Defaults to "".
+
+    Returns:
+        int: The number of characters successfully written to the file.
+    """
+    # Open file in 'w' (write/overwrite) mode with UTF-8 encoding.
     with open(filename, mode='w', encoding='utf-8') as f:
-        # Write the text content to the file.
+        # Write the text and capture the number of characters written.
         nb_characters = f.write(text)
 
-    # The f.write() method returns the number of characters written.
+    # Return the character count.
     return nb_characters
