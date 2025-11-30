71d4d797336fc66b1f865c2b64a595e363c2add8
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:45:27 +0000

    sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full creation statement for the table 'first_table'.
+-- This command shows the complete SQL used to create the table, including all constraints and parameters.
+SHOW CREATE TABLE first_table;
