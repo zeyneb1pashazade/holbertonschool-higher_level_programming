1aacc9c6c98f9a7ee2b1817ae2ee6226610e34fb
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:43:12 +0000

    sql

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database 'hbtn_0c_0' from the MySQL server.
+-- The IF EXISTS clause ensures the script does not fail if the database doesn't exist.
+DROP DATABASE IF EXISTS hbtn_0c_0;
