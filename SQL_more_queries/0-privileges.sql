d465cb3f4db1e4e33dabd16c7f0d573eb52964bc
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 06:04:41 +0000

    sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Lists all privileges granted to the MySQL users 'user_0d_1' and 'user_0d_2' on localhost.
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
