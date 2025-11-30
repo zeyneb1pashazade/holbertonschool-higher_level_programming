d1c78a8a64a6fc10c44236e12c84bc51610d5165
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:48:35 +0000

    sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the number of records where id is 89 in the table 'first_table'.
+-- The COUNT(*) function counts all rows that match the condition specified in the WHERE clause.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
