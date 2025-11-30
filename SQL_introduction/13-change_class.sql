b0e87242d5d0df7390a99a309e0d9fe5fa867684
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:52:31 +0000

    sql

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Removes all records from the table 'second_table' where the score is less than or equal to 5.
+-- This effectively removes students with low scores.
+DELETE FROM second_table
+WHERE score <= 5;
