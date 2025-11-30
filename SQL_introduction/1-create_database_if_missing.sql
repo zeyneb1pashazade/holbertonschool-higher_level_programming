268d7a6e868f3d175417e557b18b9841a1ba5339
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:41:32 +0000

    sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates the database 'hbtn_0c_0' in the MySQL server.
+-- The IF NOT EXISTS clause ensures the script does not fail if the database already exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
