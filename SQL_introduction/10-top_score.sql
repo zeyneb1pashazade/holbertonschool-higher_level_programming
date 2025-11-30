d87b4b63808319b253ce7371be7d70f66f6d839f
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-30 05:50:15 +0000

    sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table 'second_table'.
+-- It displays the score and the name, ordered by the score (highest first).
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
