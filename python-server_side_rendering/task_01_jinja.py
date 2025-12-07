50ac056ad05bc1d0268c687f9ad8ab861ef5e51d
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-12-07 11:19:27 +0000

    network1

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3
-"""
-Task 01: Basic Flask application using Jinja templates
-"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the Home Page
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
+# Route for the About Page
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
+# Route for the Contact Page
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template('contact.html')
 
-if __name__ == "__main__":
-    app.run(port=5000)
+if __name__ == '__main__':
+    # Run the app on port 5000 with debugging enabled
+    app.run(debug=True, port=5000)
